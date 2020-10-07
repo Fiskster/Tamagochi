692fc35678372212a2875729b8ae773125b91f78
Axel <axel.lilja@elev.ga.ntig.se>
2020-10-07 11:10:54 +0200

    Naming Tamagochi works. Working on Threading ticks

--- Tamagochi/Program.cs
+++ Tamagochi/Program.cs
@@ -8,9 +8,26 @@ namespace Tamagochi
         {
             Tamagochi gochi = new Tamagochi();
 
-            gochi.Feed();
+            System.Console.WriteLine("Hello! I am your Tamagochi.");
+            bool correctInput = false;
+            while (correctInput == false)
+            {
+                System.Console.WriteLine("What is my name?");
+                string name = Console.ReadLine();
+                gochi.name = name;
+                System.Console.WriteLine("My name is " + name + " , are you sure? Press Y if you are sure.");
+                ConsoleKeyInfo UI = Console.ReadKey();
+                if (UI.Key == ConsoleKey.Y)
+                {
+                 correctInput = true; 
+                 
+                }
+               
+            }
+            Console.Clear();
+            gochi.Hi();
+            gochi.Tick();
 
-           
         }
     }
 }
--- Tamagochi/Tamagochi.cs
+++ Tamagochi/Tamagochi.cs
@@ -1,53 +1,67 @@
 using System;
 using System.Collections.Generic;
-
+using System.Threading;
 namespace Tamagochi
 {
     public class Tamagochi
     {
-        private int hunger = 100;
-        private int boredom;
-        
-        private List<string> words = new List<string>() {"Hi!", "Greetings.", "Bruh!", "Cringe", "Based"};
+        private int hunger = 0;
+        private int boredom = 0;
+
+        private List<string> words = new List<string>() { "Hi!", "Greetings.", "Bruh!", "Cringe", "Based" };
 
         private Random Generator = new Random();
 
-        public string name = ""; 
+        public string name = "";
 
         private bool isAlive = true;
 
-    
+        public void Tick()
+        {
+           Thread thread = new Thread(Tick);
+           thread.Start(2);
+           Thread.Sleep(15000);
+           
+            boredom = boredom + 10;
+            hunger = hunger + 10;
+        }
+
         public void Feed()
         {
-          hunger = hunger - 10;
+            hunger = hunger - Generator.Next(2,20);
         }
         public void Hi()
         {
-            string greeting = words[Generator.Next(4)];
+            string greeting = words[Generator.Next(0,5)];
             Console.WriteLine(greeting);
             ReduceBoredom();
         }
         private void ReduceBoredom()
         {
-            boredom = boredom - 10; 
+            boredom = boredom - Generator.Next(2,20);
         }
-       public void Teach(string word)
-       {
-        words.Add(word);
-        ReduceBoredom();
-       } 
-       public void PrintStats()
-       {
-           System.Console.WriteLine(hunger);
-           System.Console.WriteLine(boredom);
-           if (isAlive == true)
-           {
-            System.Console.WriteLine("Your Tamagochi is alive!");
-           }
-           else 
-           {
-             System.Console.WriteLine("Your Tamagochi is dead :(");
-           }
-       }
+        public void Teach(string word)
+        {
+            words.Add(word);
+            ReduceBoredom();
+        }
+        public void PrintStats()
+        {
+            System.Console.WriteLine(hunger);
+            System.Console.WriteLine(boredom);
+            if (isAlive == true)
+            {
+                System.Console.WriteLine("Your Tamagochi is alive!");
+            }
+            else
+            {
+                System.Console.WriteLine("Your Tamagochi is dead :(");
+            }
+        }
+        public bool GetAlive()
+        {
+            return isAlive;
+        }
+    
     }
 }
